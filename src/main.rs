a2eec6cc9ecc06156b9c91ac78145965a0d6fe9e
sprengvogel <private@private.com>
2022-08-21 09:39:02 +0200

    Db no longer in git also idiomatic error handling for storing files

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,22 @@ fn main() -> std::io::Result<()> {
     println!("{:?}", filedates); */
 
     let conn = &establish_connection();
-    store_files_in_dir(files, conn)?;
-        
+    for file_result in files {
+        match file_result.and_then(|file| store_file_in_dir(file, conn)) {
+            Err(err) => {
+                println!("Error {} ignored while storing files.", err);
+                continue;
+            },
+            Ok(()) => continue
+        }
+    }
+
     Ok(())
 }
 
-fn store_files_in_dir(files: fs::ReadDir, conn: &diesel::SqliteConnection) -> Result<(), std::io::Error> {
-    for file_result in files {
-        let file = file_result?;
-        let path = (&file).path();
-        let last_modified = DateTime::<Local>::from((&file).metadata()?.created()?).naive_local();
-        create_file_entry(conn, (&path).to_str().expect("Filename was not valid utf-8"), &last_modified);
-    }
-
+fn store_file_in_dir(file: fs::DirEntry, conn: &diesel::SqliteConnection) -> Result<(), std::io::Error> {
+    let path = (&file).path();
+    let last_modified = DateTime::<Local>::from((&file).metadata()?.created()?).naive_local();
+    create_file_entry(conn, (&path).to_str().expect("Filename was not valid utf-8"), &last_modified);
     Ok(())
 }
